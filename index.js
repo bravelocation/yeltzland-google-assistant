77f4ad87d253b458891cdaf322c8a6fd9f29de67
John Pollard <johnp@bravelocation.com>
2018-05-23 14:35:21 +0100

    Added additional intents

--- index.js
+++ index.js
@@ -50,7 +50,45 @@ app.intent('GameScore', (conv) => {
   });
 });
 
+app.intent('NextGame', (conv) => {
+  return singleGame(true).then(function(result) {
+    conv.add(result.speechOutput);
+  });
+});
 
+app.intent('LastResult', (conv) => {
+  return singleGame(false).then(function(result) {
+    conv.add(result.speechOutput);
+  });
+});
+
+app.intent('GameTimeFixture', (conv, params) => {
+  var gameTime = params.date;
+
+  return timeBasedData(gameTime).then(function(result) {
+    if (result == null || result.speechOutput == "") {
+      conv.add("No games found on that day");
+    } else {
+      conv.add(result.speechOutput);
+    }
+    
+  });
+});
+
+app.intent('GameTimeResult', (conv, params) => {
+  var gameTime = params.date;
+
+  return timeBasedData(gameTime).then(function(result) {
+    if (result == null || result.speechOutput == "") {
+      conv.add("No games found on that day");
+    } else {
+      conv.add(result.speechOutput);
+    }
+    
+  });
+});
+
+//**** Helper functions that return promises
 function teamBased(useFixtures, team) {
   return new Promise(function(resolve, reject) {
     yeltzlandSpeech.teamBased(useFixtures, team, function(result) {
@@ -66,5 +104,41 @@ function gameScore() {
     });
   });
 };
+
+function singleGame(useFixtures) {
+  return new Promise(function(resolve, reject) {
+    yeltzlandSpeech.singleGame(useFixtures, function(result) {
+      resolve(result);
+    });
+  });
+};
+
+function timeBasedData(gameTime) {
+  return new Promise(function(resolve, reject) {
+    var parsedDate = parseSingleDate(gameTime);
+    if (parsedDate) {
+      yeltzlandSpeech.timeBased(parsedDate.startTime, parsedDate.endTime, function(result) {
+        resolve(result);
+      });
+    } else {
+      resolve(null);
+    }
+  });
+};
+
+// *** Date parsing function
+function parseSingleDate(gameTime) {
+  var parseDate = new Date(gameTime);
+
+  // Find start and end of that day
+  if (parseDate) {
+    return {
+      startTime: new Date(parseDate.getFullYear(), parseDate.getMonth(), parseDate.getDate(), 0, 0, 0),
+      endTime: new Date(parseDate.getFullYear(), parseDate.getMonth(), parseDate.getDate(), 23, 59, 59)
+    };
+  } else {
+    return null;
+  }
+}
 
 exports.handler = app;
